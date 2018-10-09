22f69fe953bf17089735a2fcc5a1a73e87ced11a
yangjack1989 <200362300@student.georgianc.on.ca>
2018-10-09 15:11:07 -0400

    first commit

---
+++ Assignemnt1-project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignemnt1_project.MvcApplication" Language="C#" %>
---
+++ Assignemnt1-project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Assignemnt1_project.Startup))]
+namespace Assignemnt1_project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
